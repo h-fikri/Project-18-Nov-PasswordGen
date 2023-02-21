6ee881a24a15508e0801a39057b00c5d4fe4ab52
Husain Hidayah <husain.fikri@gmail.com>
2023-02-21 22:40:30 +0000

    refactoring

--- script.js
+++ script.js
@@ -20,7 +20,7 @@
 // testing finished
 // ---------------------------------------------- //
 // Array of special characters to be included in password
-var charactersLength = 12;
+// var charactersLength = 10;
 var specialCharacters = [
   "@",
   "%",
@@ -113,30 +113,126 @@ var upperCasedCharacters = [
 // generating password via button click
 var generateBtn = document.querySelector("#generate");
 
-// Initial setup on variables
-var characterStore = [];
+// Declaring Empty Variable for user input
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
-// Function to prompt user for password options
+// Creating a function to generate the prompts for the user
 function getPasswordOptions() {
-  var pwd = getPasswordOptions();
-  if (pwd != null) {
-    var password = genPassword(pwd);
-    alert("Your password is: " + password);
+  // This is a loop to make sure the user selects a valid password length
+  var confirmLength;
+  // this while loop will continue to prompt the user until they enter a valid number
+  while (
+    !(confirmLength = prompt(
+      "Password length must be a number between 10-64 characters. Please try again."
+    )) ||
+    confirmLength < 10 ||
+    confirmLength > 64
+  );
+
+  // var confirmLength = prompt(
+  //   "How many characters would you like your password to contain? Minimum 10 characters and maximum 64 characters"
+  // );
+  // while (confirmLength <= 9 || confirmLength >= 65 || isNaN(confirmLength)) {
+  //   alert(
+  //     "Password length must be a number between 10-64 characters. Try again"
+  //   );
+  //   var confirmLength = prompt(
+  //     "How many characters would you like your password to contain? Minimum 10 characters and maximum 64 characters"
+  //   );
+  // }
+
+  // if (confirmLength <= 9 || confirmLength >= 65) {
+  //   alert(
+  //     "Password length must be a number between 10-64 characters. Try again"
+  //   );
+  //   var confirmLength = prompt(
+  //     "How many characters would you like your password to contain?"
+  //   );
+  // } else if (isNaN(confirmLength)) {
+  //   alert(
+  //     "Password length must be a number between 10-64 characters. Try again"
+  //   );
+  //   var confirmLength = prompt(
+  //     "How many characters would you like your password to contain?"
+  //   );
+  // }
+
+  // Question prompts for user to confirm what characters they would like to include in their password
+  var confirmSpecialCharacter = confirm(
+    "Click OK to confirm if you would like to include SPECIAL characters"
+  );
+  var confirmNumericCharacter = confirm(
+    "Click OK to confirm if you would like to include NUMERIC characters"
+  );
+  var confirmLowerCase = confirm(
+    "Click OK to confirm if you would like to include LOWERCASE characters"
+  );
+  var confirmUpperCase = confirm(
+    "Click OK to confirm if you would like to include UPPERCASE characters"
+  );
+
+  // This is a loop to make sure the user selects at least one character type
+  while (
+    confirmUpperCase === false &&
+    confirmLowerCase === false &&
+    confirmSpecialCharacter === false &&
+    confirmNumericCharacter === false
+  ) {
+    alert("You must choose at least one parameter");
+    var confirmSpecialCharacter = confirm(
+      "Click OK to confirm if you would like to include SPECIAL characters"
+    );
+    var confirmNumericCharacter = confirm(
+      "Click OK to confirm if you would like to include NUMERIC characters"
+    );
+    var confirmLowerCase = confirm(
+      "Click OK to confirm if you would like to include LOWERCASE characters"
+    );
+    var confirmUpperCase = confirm(
+      "Click OK to confirm if you would like to include UPPERCASE characters"
+    );
   }
+
+  // This array will hold the user's selected characters
+  var passwordCharacters = [];
+
+  if (confirmSpecialCharacter) {
+    passwordCharacters.push(...specialCharacters);
+  }
+
+  if (confirmNumericCharacter) {
+    passwordCharacters.push(...numericCharacters);
+  }
+
+  if (confirmLowerCase) {
+    passwordCharacters.push(...lowerCasedCharacters);
+  }
+
+  if (confirmUpperCase) {
+    passwordCharacters.push(...upperCasedCharacters);
+  }
+
+  // This is an empty string to hold the password
+  var randomPassword = "";
+
+  // This is a loop to randomly select characters from the array of user selected characters
+  for (var i = 0; i < confirmLength; i++) {
+    var randomIndex = Math.floor(Math.random() * passwordCharacters.length);
+    randomPassword += passwordCharacters[randomIndex];
+  }
+
+  // This is the password that will be displayed to the user
+  console.log(randomPassword);
+  return randomPassword;
 }
 
-// Function for getting a random element from an array
-function getRandom(arr) {}
-
-// Function to generate password with user input
-function generatePassword() {}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+//Creating a function to display the generated password to the user in the browser
 function writePassword() {
-  var password = generatePassword();
+  var password = getPasswordOptions();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
