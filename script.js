6b2847cb2f8e09bb6ba427511a9accf438b58916
Husain Hidayah <husain.fikri@gmail.com>
2023-02-21 14:39:51 +0000

    add a bit of code

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 document.write("This is From Javascript.");
+
+function generatePassword(length) {
+  // Set up a character set to use
+  let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  // Initialize the password
+  let password = "";
+
+  // Loop through and generate a random character from the character set for the specified length
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+
+  // Return the generated password
+  return password;
+}
+
+let password = generatePassword(12); // Generate a 12-character password
